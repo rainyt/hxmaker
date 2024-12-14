2dbffe9dc851ad40d88a6687f008ea93c06e9308
Rainy <939029835@qq.com>
2024-12-14 18:00:01 +0800

    新增openfl的使用例子

--- src/hx/displays/BitmapData.hx
+++ src/hx/displays/BitmapData.hx
@@ -4,5 +4,10 @@ package hx.displays;
  * 位图显示对象
  */
 class BitmapData {
+	/**
+	 * 位图数据
+	 */
+	@:noCompletion private var __data:IBitmapData;
+
 	public function new() {}
 }
---
+++ src/hx/displays/IBitmapData.hx
@@ -0,0 +1,8 @@
+package hx.displays;
+
+/**
+ * 纹理接口
+ */
+interface IBitmapData {
+	public function getTexture():Dynamic;
+}

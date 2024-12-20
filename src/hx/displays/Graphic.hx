b6a1a5f0c3b7436494c5fb07515b0d1c94dc0c14
Rainy <939029835@qq.com>
2024-12-21 07:04:58 +0800

    新增Graphic的宽高测量支持

--- src/hx/displays/Graphic.hx
+++ src/hx/displays/Graphic.hx
@@ -1,5 +1,6 @@
 package hx.displays;
 
+import hx.gemo.Rectangle;
 import hx.utils.ColorUtils;
 import hx.gemo.ColorTransform;
 
@@ -8,6 +9,9 @@ import hx.gemo.ColorTransform;
  */
 class Graphic extends DisplayObject {
 	private var __beginFill:Null<Int> = null;
+
+	@:noCompletion private var __sizeDirty:Bool = false;
+	@:noCompletion private var __sizeRect:Rectangle = new Rectangle();
 
 	/**
 	 * 图形绘制命令数据
@@ -21,6 +25,7 @@ class Graphic extends DisplayObject {
 	public function beginFill(color:Int):Void {
 		__graphicDrawData.draws.push(BEGIN_FILL(color));
 		__beginFill = color;
+		__sizeDirty = true;
 	}
 
 	/**
@@ -31,6 +36,7 @@ class Graphic extends DisplayObject {
 	public function beginBitmapData(bitmapData:BitmapData, smoothing:Bool = true):Void {
 		__graphicDrawData.draws.push(BEGIN_BITMAP_DATA(bitmapData, smoothing));
 		__beginFill = null;
+		__sizeDirty = true;
 	}
 
 	/**
@@ -47,6 +53,7 @@ class Graphic extends DisplayObject {
 			colorTransform = new ColorTransform(color.r, color.g, color.b, 1);
 		}
 		__graphicDrawData.draws.push(DRAW_TRIANGLE(vertices, indices, uvs, alpha, colorTransform));
+		__sizeDirty = true;
 	}
 
 	/**
@@ -58,6 +65,7 @@ class Graphic extends DisplayObject {
 	 */
 	public function drawRect(x:Float, y:Float, width:Float, height:Float, alpha:Float = 1, ?colorTransform:ColorTransform):Void {
 		drawTriangles([x, y, x + width, y, x, y + height, x + width, y + height], [0, 1, 2, 1, 2, 3], [0, 0, 1, 0, 0, 1, 1, 1], alpha, colorTransform);
+		__sizeDirty = true;
 	}
 
 	/**
@@ -66,5 +74,41 @@ class Graphic extends DisplayObject {
 	public function clear():Void {
 		__graphicDrawData.draws = [];
 		__beginFill = null;
+		__sizeDirty = true;
+	}
+
+	override function setTransformDirty(value:Bool = true) {
+		super.setTransformDirty(value);
+		__sizeDirty = true;
+	}
+
+	override function __getRect():Rectangle {
+		if (__sizeDirty) {
+			// 这里对Graphic的尺寸进行重新测量
+			for (draw in this.__graphicDrawData.draws) {
+				switch draw {
+					case DRAW_TRIANGLE(vertices, indices, uvs, alpha, colorTransform):
+						var len = Std.int(vertices.length / 2);
+						__sizeRect.setTo(vertices[0], vertices[1], 0, 0);
+						for (i in 0...len) {
+							var tx = vertices[i * 2];
+							var ty = vertices[i * 2 + 1];
+							if (tx < __sizeRect.left) {
+								__sizeRect.left = tx;
+							} else if (tx > __sizeRect.right) {
+								__sizeRect.right = tx;
+							}
+							if (ty < __sizeRect.top) {
+								__sizeRect.top = ty;
+							} else if (ty > __sizeRect.bottom) {
+								__sizeRect.bottom = ty;
+							}
+						}
+					default:
+				}
+			}
+			__sizeDirty = false;
+		}
+		return __sizeRect;
 	}
 }

f2bb87d501007ad34397f80152dcb10cdea46b79
Rainy <939029835@qq.com>
2024-12-21 12:41:35 +0800

    初步新增scale9Grid的支持

--- src/hx/displays/Image.hx
+++ src/hx/displays/Image.hx
@@ -10,6 +10,9 @@ import hx.providers.IRootDataProvider;
 class Image extends DisplayObject implements IDataProider<BitmapData> implements IRootDataProvider<Dynamic> {
 	@:noCompletion private var __bitmapData:BitmapData;
 	@:noCompletion private var __smoothing:Bool = true;
+	@:noCompletion private var __graphic:Graphic;
+	@:noCompletion private var __scale9Grid:Rectangle;
+	@:noCompletion private var __scale9GridDirty:Bool = false;
 
 	public var root(get, set):Dynamic;
 
@@ -20,6 +23,42 @@ class Image extends DisplayObject implements IDataProider<BitmapData> implements
 	@:noCompletion private function set_root(value:Dynamic):Dynamic {
 		this.__root = value;
 		return __root;
+	}
+
+	/**
+	 * 九宫格缩放矩形，会根据矩形的left/right/top/bottom的值来裁剪位图，并拉伸填充。
+	 */
+	public var scale9Grid(get, set):Rectangle;
+
+	private function get_scale9Grid():Rectangle {
+		return __scale9Grid;
+	}
+
+	/**
+	 * 获得九宫格图已渲染的图形
+	 * @return Graphic
+	 */
+	private function getScale9GridGraphic():Graphic {
+		if (__scale9Grid != null) {
+			if (__graphic == null) {
+				__graphic = new Graphic();
+			}
+			if (__scale9GridDirty) {
+				__scale9GridDirty = false;
+				__graphic.clear();
+				// __graphic.drawTriangles();
+				__graphic.beginBitmapData(this.data);
+				__graphic.drawRect(__scale9Grid.x, __scale9Grid.y, __scale9Grid.width, __scale9Grid.height);
+			}
+		}
+		return __graphic;
+	}
+
+	private function set_scale9Grid(value:Rectangle):Rectangle {
+		__scale9Grid = value;
+		this.setDirty();
+		this.__scale9GridDirty = true;
+		return __scale9Grid;
 	}
 
 	/**

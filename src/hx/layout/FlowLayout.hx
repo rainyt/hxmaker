e466757d0e826d8baf24ed674c884eade83ca239
Rainy <939029835@qq.com>
2025-06-13 09:57:19 +0800

    修复FlowLayout正确计算

--- src/hx/layout/FlowLayout.hx
+++ src/hx/layout/FlowLayout.hx
@@ -16,6 +16,12 @@ class FlowLayout extends Layout {
 	 */
 	public var gapY:Float = 0;
 
+	public function new(gapX:Float = 0, gapY:Float = 0) {
+		super();
+		this.gapX = gapX;
+		this.gapY = gapY;
+	}
+
 	override function update(children:Array<DisplayObject>) {
 		super.update(children);
 		if (children.length == 0)
@@ -23,13 +29,19 @@ class FlowLayout extends Layout {
 		var width = children[0].parent.width;
 		var offsetX = 0.;
 		var offsetY = 0.;
+		var nextOffsetY = 0.;
 		for (object in children) {
 			object.x = offsetX;
 			object.y = offsetY;
-			offsetX += object.width + gapX;
-			if (offsetX > width) {
+			var aX = object.width + gapX;
+			var aY = object.height + gapY;
+			offsetX += aX;
+			if (nextOffsetY < offsetY + aY) {
+				nextOffsetY = offsetY + aY;
+			}
+			if (offsetX + object.width > width) {
 				offsetX = 0.;
-				offsetY += object.height + gapY;
+				offsetY = nextOffsetY;
 			}
 		}
 	}

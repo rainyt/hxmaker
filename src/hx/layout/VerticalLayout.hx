51963a41b263919df3084c84fcd5fec66bdd4079
Rainy <939029835@qq.com>
2025-07-24 14:27:01 +0800

    修复VerticalLayout的宽度可能计算不正确的问题

--- src/hx/layout/VerticalLayout.hx
+++ src/hx/layout/VerticalLayout.hx
@@ -13,7 +13,8 @@ class VerticalLayout extends Layout {
 		var offestY:Float = 0;
 		var width:Float = parent.width;
 		for (object in children) {
-			object.width = width;
+			if (@:privateAccess parent.__width != null)
+				object.width = width;
 			object.y = offestY;
 			offestY += object.height + gap;
 		}
